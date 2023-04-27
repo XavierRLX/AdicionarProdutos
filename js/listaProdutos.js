aa1cb2bf55c8e539b1f7f42f1ed9e1f277892912
Renan Xavier <renanlima2000.aer@gmail.com>
2023-04-27 11:59:21 -0300

    adc style lista

--- js/listaProdutos.js
+++ js/listaProdutos.js
@@ -1,56 +1,58 @@
 function salvarListaProdutos() {
-    const listaProdutos = document.getElementById('listaProdutos').innerHTML;
-    localStorage.setItem('listaProdutos', listaProdutos);
-  }
-  
+  const listaProdutos = document.getElementById('listaProdutos').innerHTML;
+  localStorage.setItem('listaProdutos', listaProdutos);
+}
+
+
 
 const btnAdcLista = document.querySelector('.btnAdcLista');
-  btnAdcLista.addEventListener('click', adicionarProduto);
-  
-  function adicionarProduto() {
-      const produtoLista = document.getElementById('inputLista').value;
-      const quantidadeLista = document.getElementById('inputListaQnt').value;
-      
-      // Criar elemento de lista com checkbox, botão de exclusão e botão "Peguei"
-      const li = document.createElement('div');
-      li.innerHTML =`
+btnAdcLista.addEventListener('click', adicionarProduto);
+
+function adicionarProduto() {
+  const produtoLista = document.getElementById('inputLista').value;
+  const quantidadeLista = document.getElementById('inputListaQnt').value;
+
+  // Criar elemento de lista com checkbox, botão de exclusão e botão "Peguei"
+  const li = document.createElement('div');
+  li.innerHTML = `
       <div id="produtoLista" class="content">    
       <button class="btnPeguei">Peguei</button>
-            <span>${produtoLista} (x ${quantidadeLista})</span>
-            <div class="btn_lista_produtos">
+            <span>${produtoLista} </span>
+            <span> x ${quantidadeLista}</span>
             <img src="img/adc_carrinho.png" alt="Adicionar ao Carrinho" class="btnListaAdicionaraoCarrinho"></img>
             <img src="img/del_lista.png" alt="Excluir Produto" class="btnExcluir"></img>
-            </div>
             </div>  ` ;
-      
-      // Adicionar elemento de lista à lista de produtos
-      const listaProdutos = document.getElementById('listaProdutos');
-      listaProdutos.appendChild(li);
-      
-      salvarListaProdutos();
 
-      // Limpar os campos de entrada de dados
-      document.getElementById('inputLista').value = '';
-      document.getElementById('inputListaQnt').value = '';
-      
-    
-      const btnListaAdcCar = li.querySelector('.btnListaAdicionaraoCarrinho');
+  // Adicionar elemento de lista à lista de produtos
+  const listaProdutos = document.getElementById('listaProdutos');
+  listaProdutos.appendChild(li);
 
-      btnListaAdcCar.addEventListener('click', () => {
-        const produtoNome = li.querySelector('span').textContent.split('(')[0].trim();
-        document.getElementById('produto').value = produtoNome;
-        abrirAdicionarProduto();
-        salvarListaProdutos();
-      });
-      
+  salvarListaProdutos();
 
-      const btnPeguei = li.querySelector('.btnPeguei');
-      btnPeguei.addEventListener('click', () => {
-        li.style.textDecoration = "line-through";
-      });
-      
-  
+  // Limpar os campos de entrada de dados
+  document.getElementById('inputLista').value = '';
+  document.getElementById('inputListaQnt').value = '';
+
+  adicionarEventListeners(li);
+
+
+  const btnListaAdcCar = li.querySelector('.btnListaAdicionaraoCarrinho');
+
+  btnListaAdcCar.addEventListener('click', () => {
+    const produtoNome = li.querySelector('span').textContent.split('(')[0].trim();
+    document.getElementById('produto').value = produtoNome;
+    abrirAdicionarProduto();
+    salvarListaProdutos();
+  });
+
+
+  const btnPeguei = li.querySelector('.btnPeguei');
+  btnPeguei.addEventListener('click', () => {
+    li.style.textDecoration = "line-through";
+  });
+  adicionarEventListeners(li);
 }
+
 const listaProdutos = document.getElementById('listaProdutos');
 listaProdutos.addEventListener('click', (event) => {
   if (event.target.className === 'btnExcluir') {
@@ -59,27 +61,32 @@ listaProdutos.addEventListener('click', (event) => {
   }
 });
 
-window.onload = function() {
-    const listaProdutos = document.getElementById('listaProdutos');
-    console.log('localStorage:', localStorage); // depuração
-    const listaProdutosSalva = localStorage.getItem('listaProdutos');
-    console.log('listaProdutosSalva:', listaProdutosSalva); // depuração
-    if (listaProdutosSalva) {
-      listaProdutos.innerHTML = listaProdutosSalva;
-    }
-  
-    const listaItens = listaProdutos.getElementsByTagName('li');
-    for (let i = 0; i < listaItens.length; i++) {
-      const btnListaAdcCar = listaItens[i].querySelector('.btnListaAdicionaraoCarrinho');
-      btnListaAdcCar.addEventListener('click', () => {
-        const produtoNome = listaItens[i].querySelector('span').textContent.split('(')[0].trim();
-        document.getElementById('produto').value = produtoNome;
-        abrirAdicionarProduto();
-        salvarListaProdutos();
-      });
-    }
-  };
-  
+window.onload = function () {
+  const listaProdutos = document.getElementById('listaProdutos');
+  console.log('localStorage:', localStorage);
+  const listaProdutosSalva = localStorage.getItem('listaProdutos');
+  console.log('listaProdutosSalva:', listaProdutosSalva);
+  if (listaProdutosSalva) {
+    listaProdutos.innerHTML = listaProdutosSalva;
+  }
 
-  
-  
+  const listaItens = listaProdutos.querySelectorAll('.content');   // Itera sobre todos os elementos selecionados e adiciona um listener de eventos a cada um
+  for (let i = 0; i < listaItens.length; i++) {
+    adicionarEventListeners(listaItens[i]);
+  }
+};
+
+function adicionarEventListeners(elemento) {
+  const btnListaAdcCar = elemento.querySelector('.btnListaAdicionaraoCarrinho');
+  btnListaAdcCar.addEventListener('click', () => {
+    const produtoNome = elemento.querySelector('span').textContent.split('(')[0].trim(); // Extrai o nome do produto da linha de texto do elemento "elemento"
+    document.getElementById('produto').value = produtoNome;
+    abrirAdicionarProduto();
+    salvarListaProdutos();
+  });
+
+  const btnPeguei = elemento.querySelector('.btnPeguei');
+  btnPeguei.addEventListener('click', () => {
+    elemento.style.textDecoration = "line-through";
+  });
+}
